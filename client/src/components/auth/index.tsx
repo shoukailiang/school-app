e2cfe857a0d5d066bf7b0e2951d2dd6c59425529
shoukailiang <shoukailiang@qq.com>
2023-08-06 16:40:22 +0800

    加入了验证机制

--- client/src/components/auth/index.tsx
+++ client/src/components/auth/index.tsx
@@ -2,24 +2,30 @@ import { useEffect } from "react";
 import { getUserInfoService } from "@/services/user";
 import { loadDataReducer } from "@/store/userReducer";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Toast } from "antd-mobile";
 const Auth = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const location = useLocation();
+  const publicList = ["/register", "/login"];
   useEffect(() => {
+    if (publicList.indexOf(location.pathname) !== -1) {
+      return null;
+    }
     // 加载用户信息
-      getUserInfoService()
-        .then((res: any) => {
-          // 导航到主页
-          navigate("/me");
-          dispatch(loadDataReducer(res as any));
-          Toast.show("登陆成功");
-        })
-        .catch((err) => {
-          console.log(err);
-          Toast.show("获取信息失败");
-        });
+    getUserInfoService()
+      .then((res: any) => {
+        // 导航到主页
+        navigate("/me");
+        dispatch(loadDataReducer(res as any));
+        Toast.show("登陆成功");
+      })
+      .catch((err) => {
+        console.log(err);
+        Toast.show("获取信息失败");
+        navigate("/login");
+      });
   }, []);
   return null;
 };
